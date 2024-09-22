ae38d36d16ee5d0648aeac45b4c05bfa4832ab62
Crosstyan <crosstyan@outlook.com>
2024-09-22 22:48:18 +0800

    pi estimate

---
+++ client/addon.js
@@ -0,0 +1,48 @@
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
+const addon = require("../build/Release/addon.node")
+
+const calculations = process.argv[2] || 100000000
+
+function printResult(type, pi, ms) {
+  console.log(type, 'method:')
+  console.log('\tπ ≈ ' + pi +
+    ' (' + Math.abs(pi - Math.PI) + ' away from actual)')
+  console.log('\tTook ' + ms + 'ms')
+  console.log()
+}
+
+function runSync() {
+  const start = Date.now()
+  // Estimate() will execute in the current thread,
+  // the next line won't return until it is finished
+  const result = addon.calculateSync(calculations)
+  printResult('Sync', result, Date.now() - start)
+}
+
+function runAsync() {
+  // how many batches should we split the work in to?
+  const batches = process.argv[3] || 16
+  let ended = 0
+  let total = 0
+  const start = Date.now()
+
+  function done(err, result) {
+    total += result
+
+    // have all the batches finished executing?
+    if (++ended === batches) {
+      printResult('Async', total / batches, Date.now() - start)
+    }
+  }
+
+  // for each batch of work, request an async Estimate() for
+  // a portion of the total number of calculations
+  for (var i = 0; i < batches; i++) {
+    addon.calculateAsync(calculations / batches, done)
+  }
+}
+
+runSync()
+runAsync()
